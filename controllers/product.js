08784804f96564f7c22f0d74b3b2274e5e971784
a1a2a3a4a5 <linub7@gmail.com>
2023-01-10 11:07:59 +0330

    make change in getSingleProduct

--- controllers/product.js
+++ controllers/product.js
@@ -5,10 +5,39 @@ const factory = require('./handlerFactory');
 
 exports.getAllProducts = factory.getAll(Product);
 
-exports.getSingleProduct = factory.getSingleOne(Product, [
-  { path: 'category' },
-  { path: 'subcategories' },
-]);
+exports.getSingleProduct = asyncHandler(async (req, res, next) => {
+  const {
+    params: { slug },
+    query: { style, size },
+  } = req;
+  const product = await Product.findOne({ slug });
+  if (!product)
+    return next(new AppError(`Product with ${slug} was not found`, 404));
+  const discount = product.subProducts[style].discount;
+  const priceBefore = product.subProducts[style].sizes[size].price;
+  const price = discount ? priceBefore - priceBefore / discount : priceBefore;
+  return res.json({
+    status: 'success',
+    data: {
+      data: {
+        _id: product.name,
+        style: Number(style),
+        name: product.name,
+        description: product.description,
+        slug: product.slug,
+        sku: product.subProducts[style].sku,
+        brand: product.brand,
+        shipping: product.shipping,
+        images: product.subProducts[style].images,
+        color: product.subProducts[style].color,
+        size: product.subProducts[style].sizes[size].size,
+        price,
+        priceBefore,
+        quantity: product.subProducts[style].sizes[size].qty,
+      },
+    },
+  });
+});
 
 exports.updateProduct = factory.updateOne(Product);
 
